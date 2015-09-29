7bf7cdb8241c34dbbb210cc8f90e834d7b7dd0a3
Brian Hall <brian@hallmanac.com>
2015-09-29 09:34:49 -0400

    Added ability to pass in constructor parameters to the AppServiceLocator.

--- RedisRepo.Tests/ServiceLocator/IAppServiceLocator.cs
+++ RedisRepo.Tests/ServiceLocator/IAppServiceLocator.cs
@@ -5,11 +5,15 @@ namespace RedisRepo.Tests.ServiceLocator
 	public interface IAppServiceLocator
 	{
 		/// <summary>
-		/// Returns an instance based on the generic type provided.
+		///     Returns an instance based on the generic type provided.
 		/// </summary>
+		/// <param name="argumentParameters">
+		///     Constructor arguments in the particular form that is needed for a given IOC container
+		///     provider
+		/// </param>
 		/// <typeparam name="T">Type of instance needed</typeparam>
 		/// <returns></returns>
-		T GetInstance<T>() where T : class;
+		T GetInstance<T>(params object[] argumentParameters) where T : class;
 
 		/// <summary>
 		/// Gets a pre-configured instance of <typeparam name="T"></typeparam> by its instance name from the container.
